15d9f02348faf77259c0e4a2d07d6f32a683c40b
Isaac Oseas <oseasisaac91@gmail.com>
2018-11-21 02:54:09 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?('.')
+    
     
   end
 
   def question?
+    self.end_with?('?')
 
   end
 
   def exclamation?
+    self.end_with?('!')
 
   end
 
   def count_sentences
-
+    string_split = self.split('.')
+    string_split.map! |words|
+    words.split('!')
+    end
+    
+    
+  
   end
 end
